6d989dedf0611f8f5a17f2e3f91ac069d2946401
Tianlong Wang <44684053+Tian1219@users.noreply.github.com>
2018-11-11 23:08:16 -0600

    week 1.1

--- src/Week1/CaesarBreaker.java
+++ src/Week1/CaesarBreaker.java
@@ -1,5 +1,9 @@
 package Week1;
 
+import edu.duke.FileResource;
+
+import javax.lang.model.element.NestingKind;
+
 public class CaesarBreaker {
 
     public int[] countLetters(String message) {
@@ -35,20 +39,55 @@ public class CaesarBreaker {
     }
 
     public String halfOfString(String message, int start) {
-        StringBuilder half = new StringBuilder();
-        for (int i = 0; i < message.length(); i += 2) {
-            char a = message.charAt(i);
-            half.append(a);
+        String empty0 = "";
+        String empty1 = "";
+        for (int i = 0; i < message.length(); i++) {
+            if (i % 2 == 0) {
+                empty0 = empty0 + message.charAt(i);
+            } else empty1 = empty1 + message.charAt(i);
         }
-        return half.toString();
+
+        if (start == 0) return empty0;
+        else return empty1;
     }
 
     public int getKey(String s) {
-        int [] freqs = countLetters(s);
+        int[] freqs = countLetters(s);
         int maxDex = maxIndex(freqs);
-        int dkey = maxDex-4;
-        if (dkey < 0) dkey = 26 - (4 - maxDex);
+        int dkey = maxDex - 4;
+        if (dkey < 0) {
+            dkey = 26 - (4 - maxDex);
+        }
         return dkey;
     }
 
-}
+    public String decrypTwoKeys(String s) {
+        CaesarCipher cc = new CaesarCipher();
+        String firstHalf = halfOfString(s, 0);
+        String secondHalf = halfOfString(s, 1);
+        int getkey1 = getKey(firstHalf);
+        int getkey2 = getKey(secondHalf);
+        System.out.println(getkey1 + " " + getkey2);
+
+        String newString1 = cc.encrypt(firstHalf, 26 - getkey1);
+        String newString2 = cc.encrypt(secondHalf, 26 - getkey2);
+        StringBuilder sb = new StringBuilder(newString1 + newString2);
+
+        for (int i = 0; i < newString1.length(); i++) {
+            sb.setCharAt(2 * i, newString1.charAt(i));
+        }
+
+        for (int i = 0; i < newString2.length(); i++) {
+            sb.setCharAt(i * 2 + 1, newString2.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    public void testdecryptTwoKeys() {
+        CaesarCipher cc = new CaesarCipher();
+        FileResource fr = new FileResource();
+        String encrypted = fr.asString();
+        System.out.println(decrypTwoKeys(encrypted));
+    }
+
+    }
--- src/Week1/Main.java
+++ src/Week1/Main.java
@@ -10,15 +10,15 @@ public class Main {
       //  System.out.println(test.isVowel('o'));
       //  System.out.println(test.replaceVowels("Iello World",'i'));
        CaesarCipher test2 = new CaesarCipher();
-       String A = test2.encrypt("eeeens",25);
-      System.out.println(A);
+      String A = test2.encryptTwoKeys("Top ncmy qkff vi vguv vbg ycpx",26-2,26-20);
+     System.out.println(A);
     //    System.out.println(test2.encrypt("fjwp",17));
-           // WordLengths testw = new WordLengths();
-            //        testw.testCountWordKengths(new int[31]);
-      CaesarBreaker test3 = new CaesarBreaker();
-        String B = test3.decrypt(A);
-        System.out.println(B);
-
+   //        WordLengths testw = new WordLengths();
+    //              testw.testCountWordKengths(new int[31]);
+  //   CaesarBreaker test3 = new CaesarBreaker();
+  //    String B = test3.decrypTwoKeys("Akag tjw Xibhr awoa aoee xakex znxag xwko");
+  //     System.out.println(B);
+       // test3.testdecryptTwoKeys();
 
     }
 }
